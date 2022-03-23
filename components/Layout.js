350243bc807293fc43d6cd971fc322a16bde6890
Lucas Mace <lucasmace4130@gmail.com>
2022-03-22 23:01:30 -0500

    First Commit

---
+++ components/Layout.js
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from "../styles/Layout.module.css";
+
+const Layout = ({ children }) => {
+    return (
+        <div>
+            <Head>
+                <title>Lucas Mace Photography</title>
+            </Head>
+            <header>
+
+                <nav>
+                    <Link href="/"><h1>Lucas Mace Photography</h1></Link>
+                    <Link href="/about">About</Link>
+                    <Link href="/photos">Photos</Link>
+                    <Link href="/contact">Contact</Link>
+                </nav>
+            </header>
+            <main>
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default Layout;
---
+++ pages/404.js
@@ -0,0 +1,5 @@
+export default function Custom404() {
+    return (
+        <h2>Nothing to see here...</h2>
+    )
+}
