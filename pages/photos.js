57781d1eb2346151e7357c5c5c17364ec32daa04
Lucas Mace <lucasmace4130@gmail.com>
2022-03-29 22:19:52 -0500

    Albums Animating

--- pages/photos.js
+++ pages/photos.js
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import { useState } from "react";
 import styles from "../styles/Photos.module.css";
 
 export default function Photos(props) {
+    const [explode, setExplode] = useState();
+
     const skewMe = () => {
         const deg = 10;
         const min = deg * -1;
@@ -9,11 +12,43 @@ export default function Photos(props) {
         return (Math.random() * (max - min) + min).toFixed(2);
     }
 
+    const handleMouseOver = (e) => {
+        console.log({ in: e.target })
+        const allPhotos = [].slice.call(e.target.children);
+        allPhotos.splice(allPhotos.length - 1, 1);
+
+        setExplode(allPhotos);
+
+        try {
+            allPhotos[0].style.left = "-5rem";
+            allPhotos[1].style.right = "-5rem";
+            allPhotos[2].style.bottom = "-5rem";
+        } catch (e) {
+            console.error(e);
+        }
+
+    }
+    const handleMouseOut = (e) => {
+        console.log({ out: e.target });
+        const allPhotos = [].slice.call(e.target.children);
+        allPhotos.splice(allPhotos.length - 1, 1);
+
+        try {
+            explode[0].style.left = "0";
+            explode[1].style.right = "0";
+            explode[2].style.bottom = "0";
+        } catch (e) {
+            console.error(e);
+        }
+
+
+    }
+
     if (props) {
         return (
             <div className={styles.albumContainer}>
                 {props.allAlbums?.map((album, i) => (
-                    <div key={album} className={styles.albumCard}>
+                    <div key={album} onMouseEnter={handleMouseOver} onMouseLeave={handleMouseOut} className={styles.albumCard}>
                         <div className={styles.albumStack}>
                             {props?.allPhotos[i].map(photo => (
                                 <img key={photo.id} src={`./photos/${photo.filename}`} className={styles.albumStackPhoto} style={{ transform: `rotate(${skewMe()}deg)` }} />
